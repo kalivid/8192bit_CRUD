0702a48b1e23f41f2f19119a0f0d33c1fb40dcc7
kalivid <13264589472@163.com>
2017-12-14 20:09:00 +0800

    配置项目启动自动加载运行同名文件

--- apps.py
+++ apps.py
@@ -1,5 +1,8 @@
 from django.apps import AppConfig
-
+from django.utils.module_loading import autodiscover_modules
 
 class BitCrudConfig(AppConfig):
     name = 'bit_CRUD'
+
+    def ready(self):
+        autodiscover_modules('bit_CRUD')
